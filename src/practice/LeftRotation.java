51cfb4be4a93e44960d09db6a81a403110f00a66
Chetana, Chunduru <chetana.chunduru@optum.com>
2022-02-01 16:28:08 +0530

    LeftRotation

--- src/practice/LeftRotation.java
+++ src/practice/LeftRotation.java
@@ -1,13 +1,24 @@
 package practice;
-import java.util.*;
 
 public class LeftRotation {
     public static void main(String[] args) {
-
-
+        int[] arr = {1,2,3,4,5};
+        int d = 4;
+        LeftRotation lr = new LeftRotation();
+        int[] result = lr.rotateLeft(arr, d);
+        for(int i=0; i<result.length; i++){
+            System.out.print(result[i]);
+        }
     }
 
-    public List<Integer> rotateLeft(){
+    public int[] rotateLeft(int[] arr, int d){
+        int n = arr.length;
+        int mod = d%n ;
+        int[] new_arr = new int[n];
+        for(int i=0; i<n; i++){
+            new_arr[i] = arr[(i+mod)%n];
+        }
 
+        return new_arr;
     }
 }
